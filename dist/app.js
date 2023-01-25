fb9e3a93587b93c50a5c59f4a66d11b825420eab
Dibya Dahal <102875464+Killerbee7@users.noreply.github.com>
2023-01-25 14:41:01 +0200

    number validator updates

--- dist/app.js
+++ dist/app.js
@@ -39,6 +39,26 @@ function autoBind(_, _2, descriptor) {
     };
     return adjDescriptor;
 }
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById('project-list');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-project-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent = this.type.toUpperCase() + 'PROJECTS';
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
--- src/app.ts
+++ src/app.ts
@@ -52,6 +52,35 @@ function autoBind(
     };
     return adjDescriptor;
 }
+class ProjectList{
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished'){
+        this.templateElement= document.getElementById('project-list')! as HTMLTemplateElement;
+        this.hostElement= document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(
+            this.templateElement.content,
+            true
+        );
+        this.element = importedNode.firstElementChild as HTMLElement;
+        this.element.id= `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+
+    }
+    private renderContent(){
+        const listId = `${this.type}-project-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent= this.type.toUpperCase()+'PROJECTS';
+    }
+    
+    private attach(){
+        this.hostElement.insertAdjacentElement('beforeend', this.element);
+    }
+}
 
 class ProjectInput {
     templateElement: HTMLTemplateElement;
